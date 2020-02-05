10050057ff41c31c5ca851613acc5c6af854862d
bcherrez <gb.cherrez.2019@alumnos.urjc.es>
2020-02-05 18:09:12 +0100

    session 2 fibo sum n

--- Season 1/Sesion2/fibo sum N.py
+++ Season 1/Sesion2/fibo sum N.py
@@ -2,18 +2,19 @@ def fibo(n):
     index1=0
     index2=1
     sumn= 0
+    count=0
+
     for number in range(0,n+1) :
         if number==0 :
             sumn=0
         elif number==1:
-            sumn=1
+            sumn= sumn + 1
         else:
-            sumn= index1 + index2
+            count= index1 + index2
             index1 = index2
-            index2 = sumn
+            index2 = count
+            sumn=sumn + count
     return sumn
 
-print("The 5th term of the fibonacci series is:",fibo(5))
-print("The 10th term of the fibonacci series is:",fibo(10))
-print("The 15th term of the fibonacci series is:",fibo(15))
-
+print("Sumn of the firsts 5 terms of the fibonacci series is:",fibo(5))
+print("Sumn of the firsts 10 terms of the fibonacci series is:",fibo(10))
