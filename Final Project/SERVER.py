4683da295cb6524647bfd0d92b63422e09e52e4b
bcherrez <gb.cherrez.2019@alumnos.urjc.es>
2020-05-10 23:42:29 +0200

    ##Final Project

--- Final Project/SERVER.py
+++ Final Project/SERVER.py
@@ -15,10 +15,12 @@ bases = ['A', 'C', 'T', 'G']
 
 # -- This is for preventing the error: "Port already in use"
 socketserver.TCPServer.allow_reuse_address = True
+# -- This is for preventing the error:"Limit_value referenced before assignment"
+limit_value = " "
+
 
 # Class with our Handler. It is a called derived from BaseHTTPRequestHandler
 # It means that our class inheritates all his methods and properties
-limit_value = " "
 
 
 class TestHandler(http.server.BaseHTTPRequestHandler):
@@ -26,8 +28,11 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         """This method is called whenever the client invokes the GET method in the HTTP protocol request"""
         global limit_value
+        # Print the request line
         termcolor.cprint(self.requestline, 'green')
+        # Analize the request line
         request_line = self.requestline.split(' ')
+        # Generating the response message
         arguments = (request_line[1]).split("?")
         resource = arguments[0]
         error_code = 404
@@ -38,6 +43,10 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 self.send_response(200)
 
             elif resource == '/listSpecies':
+                new_argument = arguments
+                limit = new_argument[1]
+                limit_value = limit.split("=")
+                list_species = []
                 endpoint = '/info/species'
                 conn.request("GET", endpoint + PARAMS)
                 response = conn.getresponse()
@@ -45,31 +54,15 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 ensembl_data = json.loads(data)
                 species = ensembl_data['species']
 
-                if len(arguments) > 1:
-                    new_argument = arguments
-                    limit = new_argument[1]
-                    limit_value = limit.split("=")
-                list_species = []
                 if limit_value[1] == '':
-                    for item in ensembl_data['species']:
+                    for item in species:
                         list_species.append(item['common_name'])
                 else:
-
-                    if len(limit_value) == 3:
-                        limit_value = (limit_value[1]).split('&')
-                        species = ensembl_data['species']
-                        count = 0
-                        for item in species:
-                            if count < int(limit_value[0]):
-                                list_species.append(item['common_name'])
-                                count += 1
-                    else:
-                        species = ensembl_data['species']
-                        count = 0
-                        for item in species:
-                            if count < int(limit_value[1]):
-                                list_species.append(item['common_name'])
-                                count += 1
+                    count =0
+                    for item in species:
+                        if count < int(limit_value[1]):
+                            list_species.append(item['common_name'])
+                            count += 1
                 contents = Path("list species.html").read_text()
                 contents += f"<p>The total number of species is :{len(species)}</p>"
                 if not limit_value[1] == '':
@@ -78,6 +71,7 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 for item in list_species:
                     contents += f"<p>-{item}</p>"
                 self.send_response(200)
+
             elif resource == '/karyotype':
                 new_argument = arguments
                 specie = new_argument[1]
@@ -96,18 +90,17 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 karyotype_list = []
                 for item in ensembl_data['karyotype']:
                     karyotype_list.append(item)
-
                 contents = Path("karyotype.html").read_text()
                 contents += f"<h2> The karyotype of the specie {specie_name[-1]} is: </h2>"
                 for chromosomes in karyotype_list:
                     contents += f"<p>{chromosomes}</p>"
                 self.send_response(200)
+
             elif resource == "/chromosomeLength":
                 new_argument = arguments[1]
                 chromo_n = new_argument.split("=")
                 specie_n = chromo_n[1].split("&")
                 chromosome = chromo_n[-1]
-
                 endpoint = '/info/assembly/'
                 params = specie_n[0] + PARAMS
                 conn.request("GET", endpoint + params)
@@ -121,14 +114,15 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 contents = Path("chromosome length.html").read_text()
                 contents += f"<p>The length of the {specie_n[0]} chromosome {chromo_n[-1]} is: {chromosome_length}</p>"
                 self.send_response(200)
+
             elif resource == "/geneList":
                 new_argument = arguments[1]
                 seq_argument = new_argument.split("&")
                 params_n = []
-
                 for e in seq_argument:
                     e = e.split('=')
                     params_n.append(e[-1])
+
                 endpoint = "/overlap/region/human/"
                 params = params_n[0] + ":" + params_n[1] + "-" + params_n[
                     2] + "?feature=gene;content-type=application/json"
@@ -139,10 +133,8 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 gene_list = []
                 for element in gene_d:
                     gene_list.append(element['external_name'])
-
                 contents = Path("gene list.html").read_text()
                 contents += f"<h2>The genes in the chromosome {params_n[0]} starting at {params_n[1]} and ending at {params_n[2]} are:</h2>"
-
                 for genes in gene_list:
                     contents += f"<p>{genes}</p>"
                 self.send_response(200)
@@ -198,18 +190,17 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                         self.send_response(200)
 
         except(KeyError, ValueError, IndexError, TypeError):
+            # Generating the response message
             contents = Path('Error.html').read_text()
             self.send_response(error_code)
         else:
+            # Define the content-type header:
             self.send_header('Content-Type', 'text/html')
             self.send_header('Content-Length', len(str.encode(contents)))
-
         # The header is finished
         self.end_headers()
-
         # Send the response message
         self.wfile.write(str.encode(contents))
-
         return
 
 
@@ -223,8 +214,7 @@ Handler = TestHandler
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("Serving at PORT", PORT)
 
-    # -- Main loop: Attend the client. Whenever there is a new
-    # -- clint, the handler is called
+    # -- Main loop: Attend the client. Whenever there is a new client, the handler is called
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
