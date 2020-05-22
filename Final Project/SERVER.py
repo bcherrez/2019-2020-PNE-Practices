9cd841e22ba8d6dc97286d9464eb29505f928102
bcherrez <gb.cherrez.2019@alumnos.urjc.es>
2020-05-22 16:36:42 +0200

    ##Final Project

--- Final Project/SERVER.py
+++ Final Project/SERVER.py
@@ -38,7 +38,6 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
             if resource == "/":
                 contents = Path('Main page.html').read_text()
                 self.send_response(200)
-
             elif resource == '/listSpecies':
                 new_argument = arguments
                 limit = new_argument[1]
@@ -50,7 +49,6 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 data = response.read().decode("utf-8")
                 ensembl_data = json.loads(data)
                 species = ensembl_data['species']
-
                 if limit_value[1] == '':
                     for item in species:
                         list_species.append(item['common_name'])
@@ -60,15 +58,24 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                         if count < int(limit_value[1]):
                             list_species.append(item['common_name'])
                             count += 1
-                contents = Path("list species.html").read_text()
-                contents += f"<p>The total number of species is :{len(species)}</p>"
-                if not limit_value[1] == '':
-                    contents += f"<p>The limit you have selected is: {limit_value[-1]}</p>"
-                contents += f"<p>The name of the species are: "
-                for item in list_species:
-                    contents += f"<p>-{item}</p>"
-                self.send_response(200)
-
+                if 'json=1' in request_line[1]:
+                    if limit_value[1] == '':
+                        json_data ={'ListSpecies': list_species}
+                        contents = json.dumps(json_data)
+                        self.send_response(200)
+                    else:
+                        json_data = {'ListSpecies': {'Limit': limit_value[0], 'Species': list_species}}
+                        contents = json.dumps(json_data)
+                        self.send_response(200)
+                else:
+                    contents = Path("list species.html").read_text()
+                    contents += f"<p>The total number of species is :{len(species)}</p>"
+                    if not limit_value[1] == '':
+                        contents += f"<p>The limit you have selected is: {limit_value[-1]}</p>"
+                    contents += f"<p>The name of the species are: "
+                    for item in list_species:
+                        contents += f"<p>-{item}</p>"
+                    self.send_response(200)
             elif resource == '/karyotype':
                 new_argument = arguments
                 specie = new_argument[1]
@@ -87,10 +94,14 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 karyotype_list = []
                 for item in ensembl_data['karyotype']:
                     karyotype_list.append(item)
-                contents = Path("karyotype.html").read_text()
-                contents += f"<h2> The karyotype of the specie {specie_name[-1]} is: </h2>"
-                for chromosomes in karyotype_list:
-                    contents += f"<p>{chromosomes}</p>"
+                if 'json=1' in request_line[1]:
+                    json_data = {'species': {specie_argument: {'chromosomes': karyotype_list}}}
+                    contents = json.dumps(json_data)
+                else:
+                    contents = Path("karyotype.html").read_text()
+                    contents += f"<h2> The karyotype of the specie {specie_name[-1]} is: </h2>"
+                    for chromosomes in karyotype_list:
+                        contents += f"<p>{chromosomes}</p>"
                 self.send_response(200)
 
             elif resource == "/chromosomeLength":
@@ -108,8 +119,13 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 for item in ensembl_data['top_level_region']:
                     if item['name'] == chromosome:
                         chromosome_length = str(item['length'])
-                contents = Path("chromosome length.html").read_text()
-                contents += f"<p>The length of the {specie_n[0]} chromosome {chromo_n[-1]} is: {chromosome_length}</p>"
+                if 'json=1' in request_line[1]:
+                    json_data = {'species': {specie_n[0]: {
+                        'Chromosome': {chromosome: {'The length of the chromosome is:': chromosome_length}}}}}
+                    contents = json.dumps(json_data)
+                else:
+                    contents = Path("chromosome length.html").read_text()
+                    contents += f"<p>The length of the {specie_n[0]} chromosome {chromo_n[-1]} is: {chromosome_length}</p>"
                 self.send_response(200)
 
             elif resource == "/geneList":
@@ -134,12 +150,19 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 contents += f"<h2>The genes in the chromosome {params_n[0]} starting at {params_n[1]} and ending at {params_n[2]} are:</h2>"
                 for genes in gene_list:
                     contents += f"<p>{genes}</p>"
+                if 'json=1' in request_line[1]:
+                    json_data = {'Chromosome': params_n[0], 'Start': params_n[1],
+                                 'End': params_n[2], 'Genes': gene_list}
+                    contents = json.dumps(json_data)
                 self.send_response(200)
 
             else:
                 new_argument = arguments[1]
                 sequence_arguments = new_argument.split("=")
                 gene_name = sequence_arguments[1]
+                if 'json' in sequence_arguments[1]:
+                    arguments_json = sequence_arguments[1].split('&')
+                    gene_name = arguments_json[0]
 
                 endpoint_1 = '/xrefs/symbol/homo_sapiens/'
                 params_1 = gene_name + PARAMS
@@ -162,8 +185,12 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 if resource == "/geneSeq":
                     gene_seq = ''
                     gene_seq += ensembl_data['seq']
-                    contents += f"<h2>The sequence of the gene {gene_name} is : </h2>"
-                    contents += f"<textarea readonly rows = 20 cols = 90>{gene_seq}</textarea>"
+                    if 'json=1' in request_line[1]:
+                        json_data = {'Gene name:': {gene_name: {'Sequence': gene_seq}}}
+                        contents =json.dumps(json_data)
+                    else:
+                        contents += f"<h2>The sequence of the gene {gene_name} is : </h2>"
+                        contents += f"<textarea readonly rows = 20 cols = 90>{gene_seq}</textarea>"
                 if resource == '/geneInfo':
                     endpoint_3 = '/lookup/id/'
                     params_3 = gene_id + PARAMS
@@ -172,28 +199,51 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                     data = response_3.read().decode("utf-8")
                     ensembl_seq = json.loads(data)
                     sequence = Seq(ensembl_data['seq'])
-                    contents += f"<h2>Information about the gene: {gene_name}</h2>"
-                    contents += f"<p>Start: {ensembl_seq['start']}</p>"
-                    contents += f"<p>End: {ensembl_seq['end']}</p>"
-                    contents += f"<p>Length: {sequence.len()}</p>"
-                    contents += f"<p>In chromosome: {ensembl_seq['seq_region_name']}</p>"
-                    contents += f"<p>ID: {gene_id}</p>"
+                    if 'json=1' in request_line[1]:
+                        json_data = {'Gene': gene_name, 'Start': ensembl_seq ['start'],
+                                     'End': ensembl_seq ['end'],
+                                     'Length': sequence.len(), 'Chromosome': ensembl_seq ['seq_region_name'],
+                                     'ID': gene_id}
+                        contents = json.dumps(json_data)
+                    else:
+                        contents += f"<h2>Information about the gene: {gene_name}</h2>"
+                        contents += f"<p>Start: {ensembl_seq['start']}</p>"
+                        contents += f"<p>End: {ensembl_seq['end']}</p>"
+                        contents += f"<p>Length: {sequence.len()}</p>"
+                        contents += f"<p>In chromosome: {ensembl_seq['seq_region_name']}</p>"
+                        contents += f"<p>ID: {gene_id}</p>"
                 if resource == "/geneCalc":
                     sequence = Seq(ensembl_data['seq'])
-                    contents += f"<h2>Calculations of the gene: {gene_name}</h2>"
-                    contents += f"<p>Length: {sequence.len()}</p>"
-                    for element in bases:
-                        contents += f"<p>{element} : {sequence.count_base(element)} ({round(sequence.count_base(element) * (100 / sequence.len()), 3)}%)</p>"
-                        self.send_response(200)
+                    if 'json=1' in request_line[1]:
+                        for item in bases:
+                            contents += f"<p>{item} : {sequence.count_base(item)} ({round(sequence.count_base(item) * (100 / sequence.len()), 2)}%)</p> "
+                        json_data = {'Gene': gene_name, 'Length': sequence.len(), 'A': {sequence.count_base('A'): {
+                                'Percentage': (round(sequence.count_base('A') * (100 / sequence.len()), 2))}}, 'C': {
+                            sequence.count_base('C'): {
+                                'Percentage': (round(sequence.count_base('C') * (100 / sequence.len()), 2))}}, 'T': {
+                            sequence.count_base('T'): {
+                                'Percentage': (round(sequence.count_base('T') * (100 / sequence.len()), 2))}}, 'G': {
+                            sequence.count_base('G'): {
+                                'Percentage': (round(sequence.count_base('G') * (100 / sequence.len()), 2))}}}
+                        contents = json.dumps(json_data)
+                    else:
+                        contents += f"<h2>Calculations of the gene: {gene_name}</h2>"
+                        contents += f"<p>Length: {sequence.len()}</p>"
+                        for item in bases:
+                            contents += f"<p>{item} : {sequence.count_base(item)} ({round(sequence.count_base(item) * (100 / sequence.len()), 3)}%)</p>"
+                self.send_response(200)
 
         except(KeyError, ValueError, IndexError, TypeError):
             # Generating the response message
             contents = Path('Error.html').read_text()
             self.send_response(error_code)
+        if 'json=1' in request_line[1]:
+            self.send_header('Content-Type', 'application/json')
+            self.send_header('Content-Length', len(str.encode(contents)))
         else:
-            # Define the content-type header:
             self.send_header('Content-Type', 'text/html')
             self.send_header('Content-Length', len(str.encode(contents)))
+
         # The header is finished
         self.end_headers()
         # Send the response message
